201ef535040ab885b82c1d2ac9f91ed00cf94f11
haidong_zhang <haidong_zhang13@163.com>
2015-06-12 15:58:33 +0800

    Search

--- HumanoidBrowser1/humanoidFirefox.py
+++ HumanoidBrowser1/humanoidFirefox.py
@@ -1,5 +1,5 @@
 #!usr/bin/env python
-#coding :utf-8
+#coding:utf-8
 
 import time
 from WebRender import WebRender
@@ -13,7 +13,17 @@ class CHumanoidBrowser:
         self.url = url
         WebRender.get(url)
 
-
+    def search(self, WebRender, text):
+        # '''执行搜索功能'''
+        try:
+            if len(text) == 0:
+                return True
+            search = WebRender.find_element_by_class_name("form-control")
+            search.send_keys(text)
+            button = WebRender.find_element_by_xpath("//input[@class='btn btn-default']")
+            button.click()
+        except:
+            return False
 
 
 
@@ -21,10 +31,16 @@ def main(url):
     mBrowser = CHumanoidBrowser()
     mWebRender = WebRender.CWebRender()
     mBrowser.getPage(url, mWebRender)
+    # fp = open('search.txt')
+    # text = fp.readline()
+    # fp.close()
+    text = u'西安'
+    mBrowser.search(mWebRender, text)
+    mWebRender.closeUrl()
 
 
 
 if __name__ == '__main__':
-    url = ''
+    url = 'http://know.bkw100.com'
     main(url)
 
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # KnowledgeTree
 A project called "knowledge tree" for processing data, but this repository is not about tree structure.
+
+
+Url: http://know.bkw100.com
